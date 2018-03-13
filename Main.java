c6c96d742e4acbfb49d41221396fe66c1555d2cb
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-03-13 21:36:28 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-import Services.ClientOrder;
+
 import restaurantObjects.RestaurantTable;
 import restaurantObjects.Restaurant;
+import services.AuxilaryService;
 import servicesHolder.Holder;
-
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -10,16 +10,14 @@ public class Main {
     public static void main(String[] args) {
         HashSet<String> menu = new HashSet<>(Arrays.asList("Soup", "Steak", "Desert", "Wine"));
         Restaurant restaurant = new Restaurant();
-        int i;
-        for ( i = 0; i < 5; i++) {
-            restaurant.getTablesList().add(new RestaurantTable(menu));
-        }
+        int i = 5;
+        AuxilaryService.fillRestaurant(restaurant, menu, i);
 
         while (i > 0) {
-        Holder.getClientOrder().createOrder(restaurant);
-        Holder.getGeneralOrders().createGeneralOrder(restaurant.getTablesList());
-        Holder.getGeneralOrders().seeGeneralOrder();
-        i--;
+            Holder.getClientOrder().createOrder(restaurant);
+            Holder.getGeneralOrders().createGeneralOrder(restaurant.getTablesList());
+            AuxilaryService.seeGeneralOrder(Holder.getGeneralOrders());
+            i--;
         }
     }
 }
